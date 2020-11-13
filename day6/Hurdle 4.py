e1eb5b67d629d5182cffa23e6288ad4cf9f086c3
Ranjit Solomon <ranjitsolo@gmail.com>
2020-11-12 21:37:48 -0500

    day6 added

---
+++ day6/Hurdle 4.py
@@ -0,0 +1,25 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if (wall_on_right() and not wall_in_front()) and not right_is_clear():
+        move()
+    elif wall_in_front() and not right_is_clear():
+        turn_left()
+    elif right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+        move()
+        
+      
+
+
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
---
+++ day6/day6.py
@@ -0,0 +1,15 @@
+# define the function
+def my_function():
+    print("Hello ")
+    print("World! ")
+
+# Call the function
+my_function()
+
+# While loop
+while x < 0:
+    # Do Something
+    x -= 1
+
+while not True:
+    x -= 1
